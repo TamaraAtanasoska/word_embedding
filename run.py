39b6c561f12c780e674e9ade9762874639ef0d5d
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2021-07-12 14:35:44 +0200

    Adding terminal command structure
    Current options: --RUN_MODE (train, val, test), --CPU and --DEBUG
    RUN_MODE will determine the if training, evaluating or testing
    CPU will determine if CPU or CUDA
    DEBUG is an option that enables single example overfitting

---
+++ run.py
@@ -0,0 +1,80 @@
+import argparse
+import os
+import torch
+
+
+def parse_args():
+
+    """Parse input arguments"""
+
+    parser = argparse.ArgumentParser(description='Experiment Args')
+
+    parser.add_argument(
+        '--RUN_MODE', dest='RUN_MODE',
+        choices=['train', 'val', 'test'],
+        help='{train, val, test}',
+        type=str, required=True
+    )
+
+    parser.add_argument(
+        '--CPU', dest='CPU',
+        help='use CPU instead of GPU',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '--DEBUG', dest='DEBUG',
+        help='enter debug mode',
+        action='store_true'
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+class MainExec(object):
+    def __init__(self, args):
+        self.args = args
+
+        if self.args.CPU:
+            self.device = torch.device("cpu")
+        else:
+            self.device = torch.device(
+                "cuda" if torch.cuda.is_available() else "cpu"
+            )
+
+
+    def train(self):
+        pass
+
+
+    def eval(self):
+        pass
+
+
+    def overfit(self):
+        pass
+
+
+    def run(self, run_mode):
+        if run_mode == 'train' and self.args.DEBUG:
+            print('Overfitting a single batch...')
+            self.overfit()
+        elif run_mode == 'train':
+            print('Starting training mode...')
+            self.train()
+        elif run_mode == 'val':
+            print('Starting validation mode...')
+            self.eval()
+        elif run_mode == 'test':
+            print('Starting test mode...')
+            self.eval()
+        else:
+            exit(-1)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    exec = MainExec(args)
+    exec.run(args.RUN_MODE)
