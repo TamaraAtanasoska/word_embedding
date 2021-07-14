77307b7c7c71ceb204fa1e9801e53dee0db9abd8
Bhuvanesh-Verma <workoholic24x7@gmail.com>
2021-07-14 13:22:39 +0200

    Noise Distribution function added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import utils
 from argparse import Namespace
-args = Namespace(CKPT_EPOCH=None, CPU=False, DEBUG=False, RESUME=False, RUN_MODE='val', VERSION=None)
-data = utils.Dataset(args)
-dataloader = utils.Dataloader(dataset=data, batch_size=2)
-print(next(dataloader.get_batches()))
 
+args = Namespace(CKPT_EPOCH=None, CPU=False, DEBUG=False, RESUME=False, RUN_MODE='val', VERSION=None)
+dataset = utils.Dataset(args)
+data = dataset.get_data(split='val')
+noise_dist = utils.get_noise_dist(data)
+dataloader = utils.Dataloader(dataset=dataset, split='val', batch_size=5)
+
+print(next(dataloader.get_batches()))
+print(noise_dist.shape)
 # Better not use loop for now it will print a lot of data
 '''
 for input_words, target_words in dataloader.get_batches():
