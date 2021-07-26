3e7a4bf4c93124593f08e69abb63e84a50fe9bcc
Bhuvanesh-Verma <workoholic24x7@gmail.com>
2021-07-26 11:39:49 +0200

    ngram implemented

--- run.py
+++ run.py
@@ -69,11 +69,12 @@ class MainExec(object):
     def train(self):
         dataset = utils.Dataset(args,config)
         data = dataset.get_data(split = args.RUN_MODE)
-        vocab = dataset.get_vocab(split=args.RUN_MODE)
+        vocab = dataset.get_vocab_cls(split=args.RUN_MODE)
         ng_dist = utils.get_noise_dist(data)
-        dataloader = DataLoader(dataset, config['BATCH_SIZE'])
+        dataloader = utils.DataLoader(dataset, config['BATCH_SIZE'])
         
         data_size = len(vocab)
+        print(data_size)
         model = SkipGram(self.cfgs, data_size, ng_dist).to(self.device)
         loss_func = NegativeSamplingLoss(model, self.cfgs).to(self.device)
         optimizer = Adam(model.parameters(), lr = self.cfgs['LEARNING_RATE'])
@@ -82,17 +83,19 @@ class MainExec(object):
         model.train()
         print('Training started ...')
         for epoch in range(self.cfgs['EPOCHS']):
-            with tqdm(dataloader) as tepoch:
+            with tqdm(dataloader.get_batches()) as tepoch:
                 for step, (
                         input_words, target_words
                 ) in enumerate(tepoch):
                     # for input_words, target_words in dataloader.get_batches():
                     tepoch.set_description("Epoch {}".format(str(epoch)))
-                    inputs, targets = torch.LongTensor(input_words).to(self.device), \
-                                      torch.LongTensor(target_words).to(self.device)
+                    #inputs, targets = torch.LongTensor(input_words).to(self.device),torch.LongTensor(target_words).to(self.device)
 
+                    # Input contains list of different length, therefore cannot be converted into LongTensor at this point
+
+                    targets = torch.LongTensor(target_words).to(self.device)
                     optimizer.zero_grad()
-                    loss = loss_func(inputs, targets)
+                    loss = loss_func(input_words, targets)
                     loss.backward()
                     optimizer.step()
 
@@ -100,18 +103,11 @@ class MainExec(object):
 
                     tepoch.set_postfix(loss=loss.item())
                     sleep(0.1)
-                    writer.add_scalar('training loss', loss.item(), epoch + step)
+                   # writer.add_scalar('training loss', loss.item(), epoch + step)
                     if step % 1000 == 0:
                         print("Epoch: {}/{}".format(epoch + 1, self.cfgs['EPOCHS']))
                         print("Loss: ", loss.item())  # avg batch loss at this point in training
-                        valid_examples, valid_similarities = utils.cosine_similarity(model.in_embeddings, device=self.device)
-                        _, closest_idxs = valid_similarities.topk(6)
 
-                        valid_examples, closest_idxs = valid_examples.to('cpu'), closest_idxs.to('cpu')
-                        for ii, valid_idx in enumerate(valid_examples):
-                            closest_words = [vocab.lookup_index(idx.item()) for idx in closest_idxs[ii]][1:]
-                            print(vocab.lookup_index(valid_idx.item()) + " | " + ', '.join(closest_words))
-                        print("...\n")
             print('epoch {}, loss {}'.format(epoch, loss_sum/data_size))
 
 
@@ -150,7 +146,7 @@ class MainExec(object):
             loss.backward()
             optimizer.step()
             print('epoch {}, loss {}'.format(epoch, round(loss.item(), 3)))
-            writer.add_scalar('training loss', round(loss.item(), 3) , epoch )
+            #writer.add_scalar('training loss', round(loss.item(), 3) , epoch )
 
     def run(self, run_mode):
         if run_mode == 'train' and self.args.DEBUG:
