b88078a2ee9531a5ba78799540bd974f22bbf8e3
Tamara Atanasoska <atanasoska.tamara@gmail.com>
2021-08-03 19:46:47 +0200

    Adding GRU65, GRU350 and ZG222 datasets

--- eval/datasets.py
+++ eval/datasets.py
@@ -27,12 +27,11 @@ def get_WS353():
 def get_RG65():
     """
     Rubenstein and Goodenough similarity dataset.
-    Scores were scaled by factor 10/4.
     """
     data = _get_as_df('EN-RG-65.txt', header=None, sep="\t").values
 
     return Bunch(X=data[:, 0:2].astype("object"),
-                 y=data[:, 2].astype(np.float) * 10.0 / 4.0)
+                 y=data[:, 2].astype(np.float))
 
 def get_RW():
     """
@@ -44,3 +43,36 @@ def get_RW():
     return Bunch(X=data[:, 0:2].astype("object"),
                  y=data[:, 2].astype(np.float),
                  sd=np.std(data[:, 3:].astype(np.float)))
+
+def get_GRU65():
+    """
+    Rubenstein and Goodenough similarity dataset, German version.
+    65 pairs.
+    """
+
+    data = _get_as_df('GRU65.txt', header=None, sep="\t").values
+
+    return Bunch(X=data[:, 0:2].astype("object"),
+                 y=data[:, 2].astype(np.float))
+
+def get_GRU350():
+    """
+    Rubenstein and Goodenough similarity dataset, German version.
+    350 pairs. 
+    """
+
+    data = _get_as_df('GRU350.txt', header=None, sep="\t").values
+
+    return Bunch(X=data[:, 0:2].astype("object"),
+                 y=data[:, 2].astype(np.float))
+
+def get_ZG222():
+    """
+    Zesch and Gurevych similarity dataset, German.
+    222 pairs.
+    """
+
+    data = _get_as_df('ZG222.txt', header=None, sep="\t").values
+
+    return Bunch(X=data[:, 0:2].astype("object"),
+                 y=data[:, 2].astype(np.float))
